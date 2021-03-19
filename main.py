bbb56344e7e52c832b71bd0120506082f49ae3c4
Chris Adam <adam.chris@live.be>
2021-03-19 14:25:11 +0100

    First commit

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import os
 import pickle
 
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import tensorflow as tf
 from keras_preprocessing.image import ImageDataGenerator
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.applications.resnet import preprocess_input
@@ -10,10 +12,12 @@ from tensorflow.keras.layers import Dense, Conv2D, MaxPooling2D, Flatten, Dropou
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.optimizers import RMSprop
 
+AUTOTUNE = tf.data.AUTOTUNE
+
 
 class DogCatClassifier:
-    IMG_HEIGHT = 150
-    IMG_WIDTH = 150
+    IMG_HEIGHT = 256
+    IMG_WIDTH = 256
     BATCH_SIZE = 16
 
     def __init__(self, data="data", epochs=10):
@@ -29,26 +33,46 @@ class DogCatClassifier:
         self.model = DogCatClassifier._load_model()
 
     def train(self):
-        train_set, val_set, test_set = DogCatClassifier._gen_data(self.X, self.y)
+        train_set, val_set, test_set = self._gen_data()
 
         # Fit the model
-        self.model.fit(train_set,
-                       steps_per_epoch=train_set.n // DogCatClassifier.BATCH_SIZE,
-                       epochs=self.epochs,
-                       validation_data=val_set,
-                       validation_steps=val_set.n // DogCatClassifier.BATCH_SIZE)
+        cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath="backup/weights-{epoch:03d}.ckpt",
+                                                         save_weights_only=True,
+                                                         verbose=1)
+
+        history = self.model.fit(train_set,
+                                 steps_per_epoch=train_set.n // DogCatClassifier.BATCH_SIZE,
+                                 epochs=self.epochs,
+                                 validation_data=val_set,
+                                 validation_steps=val_set.n // DogCatClassifier.BATCH_SIZE,
+                                 callbacks=[cp_callback])
 
         # Show the predictions on the testing set
         step_size_test = test_set.n // test_set.batch_size
         self.model.evaluate(test_set, steps=step_size_test)
 
-        test_set.reset()
-        pred = self.model.predict(test_set,
-                                  steps=step_size_test,
-                                  verbose=1)
+        # Plot training results
+        acc = history.history['accuracy']
+        val_acc = history.history['val_accuracy']
 
-        print("1s:", np.multiply(pred >= 0.5, 1).sum())
-        print("0s:", np.multiply(pred < 0.5, 1).sum())
+        loss = history.history['loss']
+        val_loss = history.history['val_loss']
+
+        epochs_range = range(self.epochs)
+
+        plt.figure(figsize=(8, 8))
+        plt.subplot(1, 2, 1)
+        plt.plot(epochs_range, acc, label='Training Accuracy')
+        plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+        plt.legend(loc='lower right')
+        plt.title('Training and Validation Accuracy')
+
+        plt.subplot(1, 2, 2)
+        plt.plot(epochs_range, loss, label='Training Loss')
+        plt.plot(epochs_range, val_loss, label='Validation Loss')
+        plt.legend(loc='upper right')
+        plt.title('Training and Validation Loss')
+        plt.show()
 
     @classmethod
     def _load_model(cls):
@@ -58,40 +82,41 @@ class DogCatClassifier:
                          activation="relu", padding="same"))
         model.add(MaxPooling2D(pool_size=(2, 2)))
 
-        model.add(Conv2D(128, (3, 3), activation="relu"))
+        model.add(Conv2D(128, (3, 3), activation="relu", padding="same"))
         model.add(MaxPooling2D(pool_size=(2, 2)))
 
-        model.add(Conv2D(64, (3, 3), activation="relu"))
+        model.add(Conv2D(64, (3, 3), activation="relu", padding="same"))
         model.add(MaxPooling2D(pool_size=(2, 2)))
 
-        model.add(Conv2D(64, (3, 3), activation="relu"))
+        model.add(Conv2D(64, (3, 3), activation="relu", padding="same"))
         model.add(MaxPooling2D(pool_size=(2, 2)))
 
-        model.add(Conv2D(32, (3, 3), activation="relu"))
+        model.add(Conv2D(32, (3, 3), activation="relu", padding="same"))
         model.add(MaxPooling2D(pool_size=(2, 2)))
 
         model.add(Flatten())
-        model.add(Dense(32, activation="relu"))
-        model.add(Dropout(0.5))
+        model.add(Dense(128, activation="relu"))
+        model.add(Dropout(0.2))
         model.add(Dense(1, activation="sigmoid"))
 
         model.compile(loss='binary_crossentropy',
                       optimizer=RMSprop(lr=1e-4),
-                      metrics=['accuracy'])
+                      metrics=['accuracy', 'AUC'])
         print(model.summary())
 
         return model
 
-    @classmethod
-    def _gen_data(cls, X, y):
-        X_train, X_test, y_train, y_test = train_test_split(X, y)
+    def _gen_data(self):
+        X_train, X_test, y_train, y_test = train_test_split(self.X, self.y)
         df_train = pd.DataFrame({"filename": X_train,
                                  "class": y_train})
         df_test = pd.DataFrame({"filename": X_test,
                                 "class": y_test})
 
         train_datagen = ImageDataGenerator(rescale=1/255, validation_split=0.2, preprocessing_function=preprocess_input,
-                                           horizontal_flip=True, shear_range=0.2, fill_mode="nearest")
+                                           horizontal_flip=True, shear_range=0.2, width_shift_range=0.2,
+                                           height_shift_range=0.2, zoom_range=0.2, rotation_range=30,
+                                           fill_mode="nearest")
         test_datagen = ImageDataGenerator(rescale=1/255, preprocessing_function=preprocess_input)
 
         train_data_generator = train_datagen.flow_from_dataframe(df_train,
@@ -100,25 +125,29 @@ class DogCatClassifier:
                                                                  y_col='class',
                                                                  subset='training',
                                                                  shuffle=True,
-                                                                 batch_size=cls.BATCH_SIZE,
+                                                                 batch_size=self.BATCH_SIZE,
                                                                  class_mode='binary',
-                                                                 target_size=(cls.IMG_HEIGHT, cls.IMG_WIDTH))
+                                                                 target_size=(self.IMG_HEIGHT, self.IMG_WIDTH))
         valid_data_generator = train_datagen.flow_from_dataframe(df_train,
                                                                  directory='data',
                                                                  x_col='filename',
                                                                  y_col='class',
                                                                  subset='validation',
                                                                  shuffle=True,
-                                                                 batch_size=cls.BATCH_SIZE,
+                                                                 batch_size=self.BATCH_SIZE,
                                                                  class_mode='binary',
-                                                                 target_size=(cls.IMG_HEIGHT, cls.IMG_WIDTH))
+                                                                 target_size=(self.IMG_HEIGHT, self.IMG_WIDTH))
         test_data_generator = test_datagen.flow_from_dataframe(df_test,
                                                                directory='data',
                                                                x_col='filename',
                                                                shuffle=False,
                                                                batch_size=62,
                                                                class_mode=None,
-                                                               target_size=(cls.IMG_HEIGHT, cls.IMG_WIDTH))
+                                                               target_size=(self.IMG_HEIGHT, self.IMG_WIDTH))
+
+        # train_data_generator = train_data_generator.cache().prefetch(buffer_size=AUTOTUNE)
+        # valid_data_generator = valid_data_generator.cache().prefetch(buffer_size=AUTOTUNE)
+        # test_data_generator = test_data_generator.cache().prefetch(buffer_size=AUTOTUNE)
 
         return train_data_generator, valid_data_generator, test_data_generator
 
@@ -126,6 +155,3 @@ class DogCatClassifier:
 if __name__ == "__main__":
     clf = DogCatClassifier()
     clf.train()
-
-    with open("test", "wb") as file:
-        pickle.dump(clf, file)
