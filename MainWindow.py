24679a9f6e8419b30968272de4115bb78ac87be8
Olivier Renson <oli.renson@hotmail.com>
2021-03-31 16:18:55 +0200

    Main window class

---
+++ MainWindow.py
@@ -0,0 +1,7 @@
+from PyQt5.QtWidgets import QRadioButton, QSlider, QLabel, QMessageBox, QFileDialog
+from PyQt5.QtWidgets import QMainWindow, QWidget, QPushButton, QInputDialog
+from PyQt5 import QtCore, QtWidgets, QtGui
+
+class MyWindow(QMainWindow):
+    def __init__(self):
+        super(MyWindow, self).__init__()
